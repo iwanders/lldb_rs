6dad4cc04ed2444059ee08efe6a1645a3667607a
Ivor Wanders <ivor@iwanders.net>
2022-05-12 19:27:07 -0400

    Disable doc tests, unit tests passing now.

--- lldb/src/lib.rs
+++ lldb/src/lib.rs
@@ -2,6 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#![allow(rustdoc::broken_intra_doc_links)]
+#![allow(rustdoc::invalid_rust_codeblocks)]
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
